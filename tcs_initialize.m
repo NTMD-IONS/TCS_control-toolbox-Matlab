442932e670623af5be084d75c47a38ac070a07c2
Cedric Lenoir <cedlenoir@users.noreply.github.com>
2026-09-07 09:25:14 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- tcs_initialize.m
+++ tcs_initialize.m
@@ -13,7 +13,7 @@
 % Institute of Neuroscience (IoNS), UCLouvain, Brussels, Belgium.
 % Copyright (c) 2026 Université catholique de Louvain (UCLouvain)
 % Licensed under the MIT License. See LICENSE file for details.
-% Version 4.0.0, August 2026.//
+% Version 1.0.0, August 2026.//
 
 classdef tcs_initialize < serial_manager
 
